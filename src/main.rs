846a20996076bd9f192d7ca76898709f6e2aa289
AlexAdamov <alx.adamov@gmail.com>
2023-12-04 21:44:17 +0100

    Subscriptions and health_check endpoint running

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use newsletter::configuration::get_configuration;
+use newsletter::startup::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
+}
